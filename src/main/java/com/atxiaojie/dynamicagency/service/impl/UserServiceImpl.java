0cb4a72a7cff811f77df12a3e9955813f232c505
暮云凝香 <40419323+Zhou-xiao-jie@users.noreply.github.com>
2022-02-13 10:52:22 +0800

    Update UserServiceImpl.java

--- src/main/java/com/atxiaojie/dynamicagency/service/impl/UserServiceImpl.java
+++ src/main/java/com/atxiaojie/dynamicagency/service/impl/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.atxiaojie.dynamicagency.service.UserService;
  * @Description: 动态代理
  * @author: zhouxiaojie
  * @date: 2021/11/3 19:23
- * @copyright: 杭州鸿泉物联网技术股份有限公司
  * @Version: V1.0.0
  */
 public class UserServiceImpl implements UserService {
