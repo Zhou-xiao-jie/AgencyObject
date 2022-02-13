8b77cb29e31b1022360b78ce965ed565dc0f00c6
暮云凝香 <40419323+Zhou-xiao-jie@users.noreply.github.com>
2022-02-13 10:50:04 +0800

    Update UserServiceProxy.java

--- src/main/java/com/atxiaojie/agencyobject/service/impl/UserServiceProxy.java
+++ src/main/java/com/atxiaojie/agencyobject/service/impl/UserServiceProxy.java
@@ -7,7 +7,6 @@ import com.atxiaojie.agencyobject.service.UserService;
  * @Description: 静态代理
  * @author: zhouxiaojie
  * @date: 2021/11/3 19:16
- * @copyright: 杭州鸿泉物联网技术股份有限公司
  * @Version: V1.0.0
  */
 public class UserServiceProxy implements UserService {
