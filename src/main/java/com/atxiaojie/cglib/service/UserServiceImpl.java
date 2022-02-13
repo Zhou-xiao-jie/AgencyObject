81246fecc0d5c9fed5d8c1a347a842dab4f7ab90
暮云凝香 <40419323+Zhou-xiao-jie@users.noreply.github.com>
2022-02-13 10:51:29 +0800

    Update UserServiceImpl.java

--- src/main/java/com/atxiaojie/cglib/service/UserServiceImpl.java
+++ src/main/java/com/atxiaojie/cglib/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ package com.atxiaojie.cglib.service;
  * @Description: TODO
  * @author: zhouxiaojie
  * @date: 2021/11/4 20:07
- * @copyright: 杭州鸿泉物联网技术股份有限公司
  * @Version: V1.0.0
  */
 public class UserServiceImpl {
